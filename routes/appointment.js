90191a247edf0bfb2c8aa5a6e3dc41bd6b675c77
Nimish Verma <nimishverma@ymail.com>
2019-05-29 03:07:36 +0530

    Adds remaining basic APIs|Next:React App

--- routes/appointment.js
+++ routes/appointment.js
@@ -27,4 +27,41 @@ router.post('/appointment', (req, res) => {
     })
 })
 
+router.get('/appointment',(req, res) => {
+    // console.log(!req.query.id)
+    if(!req.query.id && !req.query.client){
+        // console.log(AppointmentModel.find())
+        AppointmentModel.find({} , (err, appointments)=>{
+            // console.log(appointments)
+            return res.status(200).send({"count":appointments.length,"data":appointments})
+        })
+    }
+    else
+    {
+        let cl_input = req.query.client
+        let phone_input = req.query.phone
+        
+        // console.log(phone_input)
+        AppointmentModel.find(req.query, (err, appointments)=>{
+            return res.status(200).send({"count":appointments.length,"data":appointments})
+        })
+    }
+})
+
+router.delete('/appointment', (req,res) => {
+    if (!req.query.id){
+        return res.status(400).send("Missing URL Parameter")
+    }
+    AppointmentModel.findByIdAndDelete(req.query.id , (err,data)=>{
+        // console.log(err==null)
+        // console.log(data)
+        if(err==null && data!=null) {return res.status(200).send("SUCCESSFULLY DELETED")}
+        else if(err==null && data==null) {
+            console.log("HERE")
+            return res.status(400).send("Object already deleted")}
+        else{
+            console.log(err)
+            return res.status(500).send("Some error occured")}
+    })
+})
 module.exports = router
